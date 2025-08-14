96d7951243302b2cf5e7b03b3ecfaf931a2658b3
HiroUgoto <goto@catfish.dpri.kyoto-u.ac.jp>
2025-08-14 15:59:05 +0900

    implement damping

--- 0X_model_response.py
+++ 0X_model_response.py
@@ -8,6 +8,6 @@ output_file = "model/Test.tf"
 model = Array.io.read_model_file(model_file)
 
 # freq_sim,resp_sim = Array.analysis.model_medium_response_py(model,fmax=3.5,nmode=5,print_flag=True,plot_flag=True)
-freq_sim,resp_sim = Array.analysis.model_transfer_function_py(model,fmax=1.5,abs=True,print_flag=True,plot_flag=True)
+freq_sim,resp_sim = Array.analysis.model_transfer_function_py(model,fmax=0.8,abs=True,print_flag=True,plot_flag=True)
 
 Array.io.output_pv_file(output_file,freq_sim,resp_sim)
--- Array/haskell.py
+++ Array/haskell.py
@@ -5,13 +5,16 @@ import warnings
 
 
 #--------------------------------------------------------#
-def set_T_matrix(vs,rho,thick,omega):
+def set_T_matrix(vs,rho,thick,h,omega):
     T = np.zeros((2,2),dtype=np.complex128)
 
-    r = (vs[0]*rho[0]) / (vs[1]*rho[1])
+    cmp0 = np.sqrt((1 + 2j*h[0])) 
+    cmp1 = np.sqrt((1 + 2j*h[1])) 
 
-    ep = np.exp( 1j*omega*thick/vs[0])
-    em = np.exp(-1j*omega*thick/vs[0])
+    r = (vs[0]*rho[0]*cmp0) / (vs[1]*rho[1]*cmp1)
+
+    ep = np.exp( 1j*omega*thick/(vs[0]*cmp0))
+    em = np.exp(-1j*omega*thick/(vs[0]*cmp1))
     cp = 0.5*(1+r)
     cm = 0.5*(1-r)
 
@@ -21,23 +24,28 @@ def set_T_matrix(vs,rho,thick,omega):
     return T
 
 #--------------------------------------------------------#
-def haskell_matrix(nlay,vs,rho,thick,omega):
+def haskell_matrix(nlay,vs,rho,thick,h,omega):
     R = np.identity(2,dtype=np.complex128)
 
-    for i in range(0,nlay-1):
-        T = set_T_matrix(vs[i:i+2],rho[i:i+2],thick[i],omega)
-        R = np.matmul(T,R)
+    if np.isscalar(h):
+        for i in range(0,nlay-1):
+            T = set_T_matrix(vs[i:i+2],rho[i:i+2],thick[i],([h,h]),omega)
+            R = np.matmul(T,R)
+    else:
+        for i in range(0,nlay-1):
+            T = set_T_matrix(vs[i:i+2],rho[i:i+2],thick[i],h[i:i+2],omega)
+            R = np.matmul(T,R)
 
     return R
 
 #--------------------------------------------------------#
-def calc_resp(nlay,vs,rho,thick,omega):
-    R = haskell_matrix(nlay,vs,rho,thick,omega)
+def calc_resp(nlay,vs,rho,thick,h,omega):
+    R = haskell_matrix(nlay,vs,rho,thick,h,omega)
     resp = 2.0 / (R[0,0]+R[0,1])
     return resp
 
 #--------------------------------------------------------#
-def calc_transfer_function_py(nlay,vs,rho,thick,fmax=20,nfreq=200,abs=True,print_flag=False,plot_flag=False):
+def calc_transfer_function_py(nlay,vs,rho,thick,h=0.0005,fmax=20,nfreq=500,abs=True,print_flag=False,plot_flag=False):
     freq = np.linspace(fmax/nfreq,fmax,nfreq)
     resp = np.zeros(nfreq,dtype=np.complex128)
 
@@ -46,7 +54,7 @@ def calc_transfer_function_py(nlay,vs,rho,thick,fmax=20,nfreq=200,abs=True,print
 
     for i,f in enumerate(freq):
         omega = 2*np.pi*f
-        resp[i] = calc_resp(nlay,vs,rho,thick,omega)
+        resp[i] = calc_resp(nlay,vs,rho,thick,h,omega)
 
         if print_flag:
             print("  {0:.2f} Hz, {1:.2f}".format(f,np.abs(resp[i])))
